f99d28218631fbc22b52f96959f87dc621b736fd
Anton Soldatov <turbolet85@gmail.com>
2023-01-29 19:03:51 +0100

    complete with controllers

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
+//import
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 
+//middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-	res.send('Task manager app');
-});
-
 app.use('/api/v1/tasks', tasks);
 
+//server params
 const port = 3000;
 
 const start = async () => {
