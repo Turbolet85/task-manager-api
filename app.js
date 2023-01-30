b7b1b6bedbc0789e8a66102b0cb196db3974eede
Anton Soldatov <turbolet85@gmail.com>
2023-01-30 22:10:40 +0100

    complete with Task manager

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-//middleware
+//express middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasks);
 
+// custom middleware
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 //server params
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
 	try {
